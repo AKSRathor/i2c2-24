6def9f8eaa5163db00ecf74e27a290343c71c982
AshuRathor <98573681+AKSRathor@users.noreply.github.com>
2024-04-01 19:37:17 +0530

    Navbar added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script'
+ 
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -12,6 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script src="https://cdn.tailwindcss.com"></Script>
     </html>
   );
 }
