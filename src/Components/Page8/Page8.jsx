b1163702adbf3102f743c1743d3a3f7c4ab82594
AshuRathor <98573681+AKSRathor@users.noreply.github.com>
2024-04-04 19:51:33 +0530

    page7 added

---
+++ src/Components/Page8/Page8.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./Page8Style.module.css"
+
+const Page8 = () => {
+  return (
+    <div>
+        <div className={`${style.pg8HDiv}`}><h1>Sponsors & Community Partners</h1></div>
+    </div>
+  )
+}
+
+export default Page8
--- src/app/page.js
+++ src/app/page.js
@@ -12,6 +12,8 @@ import Page3 from "@/Components/Page3/Page3";
 import Page4 from "@/Components/Page4/Page4";
 import Page5 from "@/Components/Page5/Page5";
 import Page6 from "@/Components/Page6/Page6";
+import Page7 from "@/Components/Page7/Page7";
+import Page8 from "@/Components/Page8/Page8";
 gsap.registerPlugin(ScrollTrigger)
 
 export default function Home() {
@@ -39,6 +41,8 @@ export default function Home() {
       <Page4/>
       <Page5/>
       <Page6/>
+      <Page7/>
+      <Page8/>
     </div>
   );
 }
