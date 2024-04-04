c117aa7d70d0fad973d8bb46b26977946ab28152
AshuRathor <98573681+AKSRathor@users.noreply.github.com>
2024-04-04 13:57:11 +0530

    Rewards added

--- src/Components/Page5/Page5.jsx
+++ src/Components/Page5/Page5.jsx
@@ -19,7 +19,8 @@ const myChallengeArr = [
     prbDesc: "Welcome to the world of Web3 gaming! Your mission, should you choose to accept it, is to create a user-friendly game distribution platform for blockchain-based games. Envision a user-friendly, entry-level gaming distribution platform based on Web3 technology, specifically tailored for new players in the blockchain gaming space. Your challenge is to create a solution that facilitates seamless game discovery, purchasing, and management using cryptocurrencies while fostering a supportive community for gamers to grow and thrive in the world of decentralized gaming Design a simple working/non working website for this problem statement.",
     prbState: "Design a simple working/non working website for this problem statement",
     challBy: "Trikon",
-    challLink: 'https://trikon.io/'
+    challLink: 'https://trikon.io/',
+    imgLink:"https://i2c2.vercel.app/_next/static/media/health.658babc2.png"
   },
   {
     head: "Education",
@@ -27,7 +28,8 @@ const myChallengeArr = [
     prbDesc: "The problem in EdTech is the lack of effective and personalized learning experiences, as well as the limited accessibility to technology, particularly in low-income areas. The challenge is to develop innovative solutions that provide high-quality, affordable, and accessible education to all learners, regardless of their background or location.",
     prbState: "",
     challBy: "I2C2 Team",
-    challLink: 'https://i2c2.tech/'
+    challLink: 'https://i2c2.tech/',
+    imgLink:"https://i2c2.vercel.app/_next/static/media/education.d778eb33.png"
   },
   {
     head: "Blockchain",
@@ -35,7 +37,8 @@ const myChallengeArr = [
     prbDesc: "Build a platform for the users to design their own avatar (e.g. https://avatarmaker.com) and mint them as an NFT on Tezos for a price based on the amount of personalization they’ve added to their avatar such as makeup, hair, facial hair, and accessories, etc. A generative NFT collection in short.",
     prbState: "",
     challBy: "Tezos",
-    challLink: 'https://tezos.com/'
+    challLink: 'https://tezos.com/',
+    imgLink:"https://i2c2.vercel.app/_next/static/media/web3.d28c3acf.png",
   },
   {
     head: "Frontend Dev",
@@ -43,7 +46,8 @@ const myChallengeArr = [
     prbDesc: "Redesign 5irechain's website with theme inspired from NFT, Dexas and all. You have to submit complete code for the website and not just figma Wireframing.",
     prbState: "Special Prize: 5ireChain will provide extra $1000 (If the project is selected by 5ireChain Judges).",
     challBy: "5ire",
-    challLink: 'https://www.5ire.org/'
+    challLink: 'https://www.5ire.org/',
+    imgLink:"https://i2c2.vercel.app/_next/static/media/frontend.5216ab6a.png",
   },
   {
     head: "Entrepreneurship",
@@ -51,14 +55,16 @@ const myChallengeArr = [
     prbDesc: "Develop an MVP for a peer-to-peer platform that connects Indian college students with mentors, industry experts, and potential employers. It should include tools for students to showcase their skills and connect with potential employers. The platform should be user-friendly, accessible on web and mobile, scalable, and prioritise creative ideas to improve students education and career prospects.",
     prbState: "",
     challBy: " I2C2 Team",
-    challLink: 'https://i2c2.tech/'
+    challLink: 'https://i2c2.tech/',
+    imgLink:"https://i2c2.vercel.app/_next/static/media/entrepreneurship.2f5ba1b6.png",
   },
   {
     head: "Miscellaneous",
     prbHead: "Bring your own awesome project",
     prbState: "",
     challBy: "",
-    challLink: ''
+    challLink: '',
+    imgLink:"https://i2c2.vercel.app/_next/static/media/miscellaneous.fd4d1007.png",
   },
 
 ]
@@ -75,8 +81,9 @@ const Page5 = () => {
           <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class={`mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5 ${style.allChallCard}`}>
               {myChallengeArr.map((i, index) => {
-                return <div key={index} class="relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+                return <div key={index} class={`relative flex w-80 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md ${style.imgCardDiv}`}>
                   <div class="relative mx-4 -mt-6 h-40 overflow-hidden rounded-xl bg-blue-gray-500 bg-clip-border text-white shadow-lg shadow-blue-gray-500/40 bg-gradient-to-r from-blue-500 to-blue-600">
+                    <img src={i.imgLink} alt="" />
                   </div>
                   <div class="p-6">
                     <h5 class="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
--- src/app/page.js
+++ src/app/page.js
@@ -11,6 +11,7 @@ import { useEffect } from "react";
 import Page3 from "@/Components/Page3/Page3";
 import Page4 from "@/Components/Page4/Page4";
 import Page5 from "@/Components/Page5/Page5";
+import Page6 from "@/Components/Page6/Page6";
 gsap.registerPlugin(ScrollTrigger)
 
 export default function Home() {
@@ -37,6 +38,7 @@ export default function Home() {
       <Page3/>
       <Page4/>
       <Page5/>
+      <Page6/>
     </div>
   );
 }
