d1c1c49e9abba907ac8d2a239e0b0843b8778003
AshuRathor <98573681+AKSRathor@users.noreply.github.com>
2024-04-04 21:26:38 +0530

    Pg8

--- src/Components/Page8/Page8.jsx
+++ src/Components/Page8/Page8.jsx
@@ -2,11 +2,23 @@ import React from 'react'
 import style from "./Page8Style.module.css"
 
 const Page8 = () => {
-  return (
-    <div>
-        <div className={`${style.pg8HDiv}`}><h1>Sponsors & Community Partners</h1></div>
-    </div>
-  )
+    return (
+        <div className={`${style.pg8Div}`}>
+            <div className={`${style.pg8HDiv}`}><h1>Sponsors & Community Partners</h1></div>
+            <div className={`${style.pg8BottomSpons}`}>
+                <div role="tablist" className="tabs tabs-bordered">
+                    <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Tab 1" />
+                    <div role="tabpanel" className="tab-content p-10">Tab content 1</div>
+
+                    <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Tab 2" checked />
+                    <div role="tabpanel" className="tab-content p-10">Tab content 2</div>
+
+                    <input type="radio" name="my_tabs_1" role="tab" className="tab" aria-label="Tab 3" />
+                    <div role="tabpanel" className="tab-content p-10">Tab content 3</div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Page8
